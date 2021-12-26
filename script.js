97f9a4c65df14be40fb5d8bf28a6939eef120906
Wilson Scott <wscott53@my.bcit.ca>
2021-12-25 20:17:26 -0800

    Revise page, add code to typer prototype function

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-// const body = document.body;
+// I am using a tutorial about JS/DOM (that uses a type/untyper for a single word)
+// and expanding off it in an effort to learn more about JS and working with the DOM.
 
-
-// let headOne = document.createElement('h1');
-// let str = "Greetings, User.";
-
-// for (let i in str) {
-
-//   setTimeout(function () {
-//     headOne.innerText += str[i];
-//     console.log(headOne)
-//     header.appendChild(headOne);
-//   }, 1000);
-// }
-
-/*Building off of online tutorials */
+// These variables store all of the strings that will be typed out by MatrixType/Typer
 const headOne = "Greetings, User."
 
+// This is the list of class names that will be cycled through for typing/inserting
+
+// This is our main function - it runs our script.
 const MatrixType = function(textElement, string, typeTime = 1000) {
   this.textElement = textElement;
   this.string = string;
+  // We start off with empty elements
   this.text = '';
-  this.letterIndex = 0;
+  this.wordIndex = 0;
   this.typeTime = parseInt(typeTime, 10);
   this.typer();
   
@@ -29,8 +21,22 @@ const MatrixType = function(textElement, string, typeTime = 1000) {
 //Typer Method
 MatrixType.prototype.typer = function() {
   // Split the string into an array of words
-  const mySentence = this.string.split("");
-  console.log(mySentence)
+  const mySentence = this.string.split(" ");
+  // set index to zero (start of sentence)
+  let currentIndex = 0;
+  // store current word in var
+  let currentWord = mySentence[currentIndex];
+  
+  // add a character
+  this.text = currentWord.substring(0, this.text.length + 1);
+
+  //output our current text - instert .text into element
+  let test = 'class=txt'
+  this.textElement.innerHTML = `<span ${test}>${this.text}</span>`;
+
+  // Type Speed
+  let typeSpeed = 300;
+  
 
 
   setTimeout(() => this.typer(), 500)
