9b99241247a323581fd30821a80ee1cf2858a64d
Wilson Scott <wscott53@my.bcit.ca>
2021-12-27 13:06:01 -0800

    update html formatting, update script variable names

--- script.js
+++ script.js
@@ -2,15 +2,24 @@
 // and expanding off it in an effort to learn more about JS and working with the DOM.
 
 // These variables store all of the strings that will be typed out by MatrixType/Typer
-// const headOne = "Greetings, User."
 
 const strList = [
   "wil@profile:~ user$ ",
-  "Initializing...",
+  "Initializing profile creation protocol... ",
   "wil@profile:~ user$ ",
-  "Complete...",
+  "Complete! ",
   "wil@profile:~ user$ ",
-  "Securing packets from main hub."
+  "Securing data packets from main hub... ",
+  "wil@profile:~ user$ ",
+  "Unpacking data packets... ",
+  "wil@profile:~ user$ ",
+  "Making cup of coffee... ",
+  "wil@profile:~ user$ ",
+  "Processing data...",
+  "wil@profile:~ user$ ",
+  "Finished processing. Initializing UI... ",
+  "wil@profile:~ user$ ",
+  "Page initialization complete! "
 ]
 
 const elementList = [
@@ -19,14 +28,24 @@ const elementList = [
   "#TermTwo",
   "#TermContentTwo",
   "#TermOne",
-  "#TermContentOne"
+  "#TermContentOne",
+  "#TermTwo",
+  "#TermContentTwo",
+  "#TermThree",
+  "#TermContentThree",
+  "#TermFour",
+  "#TermContentFour",
+  "#TermFive",
+  "#TermContentFive",
+  "#TermSix",
+  "#TermContentSix",
 ]
 
 const linkList = ["Github", "LinkedIn", "Resume"]
 
 // This is the global index variable that controls 
 // which item of each above list is being used
-let core = 0;
+let contentIndex = 0;
 
 // This is the list of class names that will be cycled through for typing/inserting
 classList = [
@@ -39,7 +58,7 @@ const MatrixType = function(textElement, string, itemIndex) {
   this.textElement = textElement;
   this.string = string;
   // Grab contents of .container class spans
-  this.text = document.querySelector(elementList[core]).innerHTML;
+  this.text = document.querySelector(elementList[contentIndex]).innerHTML;
   this.itemIndex = itemIndex;
   this.typer();
   
@@ -50,7 +69,7 @@ const MatrixType = function(textElement, string, itemIndex) {
 MatrixType.prototype.typer = function() {
   
   // No typing effect for terminal prompt text
-  if (strList[core] === "wil@profile:~ user$ ") {
+  if (strList[contentIndex] === "wil@profile:~ user$ ") {
     this.text = "wil@profile:~ user$ ";
 
   } else {
@@ -62,13 +81,17 @@ MatrixType.prototype.typer = function() {
   this.textElement.innerHTML = `<span ${elementClass}>${this.text}</span>`;
 
   // Type Speed
-  let typeSpeed = 200;
+  let typeSpeed = 100;
 
   // This is used to pace the typing to simulate a computer processing info
   // If the current character is a period, the type speed is slowed down to show
   // that information is 'loading'.
   if (this.text.charAt(this.text.length - 1) === '.') {
     typeSpeed = 1000;
+
+  //Add whitespace character after '!' to simulate processing time before screen wipe
+  } else if (this.text.charAt(this.text.length - 1) === '!') {
+    typeSpeed = 2000;
   }
 
   if (this.text != this.string) {
@@ -79,14 +102,14 @@ MatrixType.prototype.typer = function() {
 
   } else {
     //
-    core++;
-    if (core < strList.length) {
+    contentIndex++;
+    if (contentIndex < strList.length) {
       //Clear the DOM if terminal processes are 'complete' - simulate new screen
-      if (this.text.includes("Complete")) {
+      if (this.text.includes("omplete")) {
         document.querySelectorAll(".container").forEach(content => content.innerHTML = "");  
       }
 
-      new MatrixType(document.querySelector(elementList[core]), strList[core], core);
+      new MatrixType(document.querySelector(elementList[contentIndex]), strList[contentIndex], contentIndex);
     }
   }
 
