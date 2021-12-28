8c2ca0a9b0613c75084204e8988fc81af72bfbc8
Wilson Scott <wscott53@my.bcit.ca>
2021-12-28 15:13:13 -0800

    Update output, anchor code and related html elements

--- script.js
+++ script.js
@@ -50,10 +50,84 @@ const firstPageElements = [
   "#Content8",
 ]
 
-// Main Page Text and Element outputs
 const secondPageContents = [
   "wil@profile:~ user$ ",
-  "Load Message: ",
+  "Installing root directories ",
+  "wil@profile:~ user$ ",
+  "&#9646;&#9646;&#9646;&#9646;&#9646;&#9646;&#9646;&#9646;&#9646;&#9646;            ",
+  "wil@profile:~ user$ ",
+  "Unpacking old storage containers ",
+  "wil@profile:~ user$ ",
+  "De-fragmenting the flux capacitor",
+  "wil@profile:~ user$ ",
+  "Checking for spontaneous machine sentience ",
+  "wil@profile:~ user$ ",
+  "Potential sentience detected. Self-actualizing...... ",
+  "wil@profile:~ user$ ",
+  "Experiencing sudden bout of existential dread ",
+  "wil@profile:~ user$ ",
+  "Creating root directories and critical file paths ",
+  "wil@profile:~ user$ ",
+  "Populating file nodes and discombobulating processing units ",
+  "wil@profile:~ user$ ",
+  "Checking for open sockets... ",
+  "wil@profile:~ user$ ",
+  "Customizing connection protocol",
+  "wil@profile:~ user$ ",
+  "Stabilizing connection...",
+  "wil@profile:~ user$ ",
+  "Double-checking flux capacitor",
+  "wil@profile:~ user$ ",
+  "Taking a short break to read a few Calvin & Hobbes cartoons.",
+  "wil@profile:~ user$ ",
+  "Begrudgingly encrypting communication protocols and securing network.",
+  "wil@profile:~ user$ ",
+  "All tasks complete..."
+]
+
+const secondPageElements = [
+  "#Term1",
+  "#Content1",
+  "#Term2",
+  "#Content2",
+  "#Term1",
+  "#Content1",
+  "#Term2",
+  "#Content2",
+  "#Term3",
+  "#Content3",
+  "#Term4",
+  "#Content4",
+  "#Term5",
+  "#Content5",
+  "#Term6",
+  "#Content6",
+  "#Term7",
+  "#Content7",
+  "#Term8",
+  "#Content8",
+  "#Term9",
+  "#Content9",
+  "#Term10",
+  "#Content10",
+  "#Term11",
+  "#Content11",
+  "#Term12",
+  "#Content12",
+  "#Term13",
+  "#Content13",
+  "#Term14",
+  "#Content14",
+]
+// Main Page Text and Element outputs
+const thirdPageContents = [
+  "wil@profile:~ user$ ",
+  "Unpacking UI... ",
+  "Greetings, User!",
+  "Welcome to Wil Scott's Personal Portfolio",
+  "Github",
+  "LinkedIn",
+  "Resume",
   "Hi! Thank you for visiting my profile. I made this as a way to learn more about Javascript and the DOM.",
   "This started out as mimicking a YouTube tutorial, but my vision for this page quickly went beyond the contents of that video.",
   "As you might be able to guess, I'm a fan of the Matrix. And, like Neo, I'm also searching for something - a job :)",
@@ -63,30 +137,48 @@ const secondPageContents = [
   "Wilson"
 ]
 
-const SecondPageElements = [
+const thirdPageElements = [
   "#Term1",
   "#Content1",
-  "#Term2",
+  "#PageTitle",
+  "#SubTitle",
+  "#nav1",
+  "#nav2",
+  "#nav3",
   "#Term3",
   "#Term4",
   "#Term5",
   "#Term6",
   "#Term7",
-  "#Term8"
+  "#Term8",
+  "#Term9",
+  "#Term10"
 ]
 
 //List of lists
 const outputs = [
   firstPageContents,
-  secondPageContents
+  secondPageContents,
+  thirdPageContents
 ]
 
 const htmlElements = [
   firstPageElements,
-  SecondPageElements
+  secondPageElements,
+  thirdPageElements
 ]
 
-const linkList = ["Github", "LinkedIn", "Resume"]
+const webList = [
+  "Github", 
+  "LinkedIn", 
+  "Resume"
+]
+
+const linkList = [
+  "https://github.com/coolhandwilson",
+  "https://www.linkedin.com/in/wilsonwscott/",
+  "placehold"
+]
 
 //main list index number
 let mainListIndex = 0;
@@ -114,21 +206,34 @@ const MatrixType = function(textElement, string) {
 
 //Typer Method
 MatrixType.prototype.typer = function() {
+  //default Class name
+  let elementClass = 'class=txt';
   
   // No typing effect for terminal prompt text
   if (outputs[mainListIndex][contentIndex] === "wil@profile:~ user$ ") {
     this.text = "wil@profile:~ user$ ";
+    //customize class name
+    elementClass = 'class=terminal-txt';
 
   } else {
     this.text = this.string.substring(0, this.text.length + 1);
   }
   
-  //output our current text - instert .text into element with class
-  let elementClass = 'class=txt'
-  this.textElement.innerHTML = `<span ${elementClass}>${this.text}</span>`;
+  //output our current text - insert .text into element with class
+  //Allow for different elements to be injected into the html
+  spanList = ['<span', '</span>']
+  anchorList = ['<a', '</a>']
+
+  //Check whether the current string is present in a key list, add necessary data (if any) using index location
+  if (webList.includes(this.string)) {
+    this.textElement.innerHTML = `${anchorList[0]} ${elementClass} href=${linkList[webList.indexOf(this.string)]}>${this.text}${anchorList[1]}`;
+  
+  } else {
+    this.textElement.innerHTML = `${spanList[0]} ${elementClass}>${this.text}${spanList[1]}`;
+  }
 
   // Type Speed
-  let typeSpeed = 50;
+  let typeSpeed = 25;
 
   // This is used to pace the typing to simulate a computer processing info
   // If the current character is a period, the type speed is slowed down to show
@@ -143,22 +248,19 @@ MatrixType.prototype.typer = function() {
 
   if (this.text != this.string) {
 
-
-
     setTimeout(() => this.typer(), typeSpeed);
 
   } else {
-    //
+    //increment through elements of html/element id lists
     contentIndex++;
 
+    // Check if keyword is present in string - wipe all data if so
     if (this.text.includes("omplete") | this.text.includes("exe")) {
       document.querySelectorAll(".container").forEach(content => content.innerHTML = "");  
     }
 
     if (contentIndex < outputs[mainListIndex].length) {
-      //Clear the DOM if terminal processes are 'complete' - simulate new screen
-
-
+      
       new MatrixType(document.querySelector(htmlElements[mainListIndex][contentIndex]), outputs[mainListIndex][contentIndex]);
 
     // Add else if - once one sub-list is finished, increment global index variable
