0bca809e2aa6fcb0d6153ae79b4ee0a947f9e797
Wilson Scott <wscott53@my.bcit.ca>
2021-12-30 11:41:36 -0800

    Add 404.html, style sheet and implement it in server.js

--- server.js
+++ server.js
@@ -45,7 +45,10 @@ const server = http.createServer((req, res) => {
     if (err) {
       if (err.code == 'ENOENT') {
         // Page not found
-        console.log("404 Page Not Found");
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
+          res.writeHead(200, {"Content-Type": "text/html"});
+          res.end(content, 'utf-8');
+        });
 
       } else {
         // Server Error
