764ec2cc0fe318c97e7ebd1aa325fc33baa62b14
Wilson Scott <wscott53@my.bcit.ca>
2021-12-29 18:50:27 -0800

    Create nodemon script, add code to create server

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
+// Create server
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/htmlsdfh'});
+    res.end('<h1>Cheese</h1>');
+  }
+});
+
+// create port var
+const port = process.env.PORT || 8000;
+
+// Listen
+server.listen(port, () => console.log(`Listening on port ${port}`));
