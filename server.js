349b6fa6291e51cace8b1cfc85f6d320b5331fa3
Wilson Scott <wscott53@my.bcit.ca>
2021-12-29 19:41:04 -0800

    Add code to server.js to create dynamic file paths in server

--- server.js
+++ server.js
@@ -5,10 +5,53 @@ const fs = require('fs');
 
 // Create server
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/htmlsdfh'});
-    res.end('<h1>Cheese</h1>');
+  // Dynamic File Path
+  let filePath = path.join(__dirname, 'public', req.url === "/" ? 'index.html' : req.url);
+
+  // Get the extension of file
+  let extensionName = path.extname(filePath);
+
+  // Set content type
+  let contentType = 'text/html';
+
+  // Check extension and change content type accordingly
+  switch(extensionName) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
   }
+
+  // Read target file
+  fs.readFile(filePath, (err, content) => {
+    // If an error occurs
+    if (err) {
+      if (err.code == 'ENOENT') {
+        // Page not found
+        console.log("404 Page Not Found");
+
+      } else {
+        // Server Error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    // Success
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
 });
 
 // create port var
